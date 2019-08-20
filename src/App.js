c3fa77ce21543a92ba0cb4e1c93b41b11a9434e7
Pedro Rique <pedro.rique@wooza.com.br>
2019-08-20 17:39:35 -0300

    feat: muda estrategia de roteamento, com menu fixo e estrutura no App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-import Header from './components/header'
+import Header from './components/header';
+
+import Flex from './pages/flexbox';
+import Grid from './pages/grid';
 
 const App = () => (
-  <div className="App">
-      <Header />
-  </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/flexbox" component={Flex} />
+        <Route exact path="/grid" component={Grid} />
+      </div>
+    </Router>
 );
 
 export default App;
