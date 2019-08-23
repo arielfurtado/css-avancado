64646c24187076ceab1c9b62fa1c5602fffca34a
Lucio Ariel <lucio.furtado@wooza.com.br>
2019-08-23 15:00:20 -0300

    Cria html dos cards

--- src/pages/flexbox/index.js
+++ src/pages/flexbox/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
+import './style.scss';
+
 const Flex = () => (
     <div>
         <h1>Flexbox</h1>
+        <div class="cards">
+            <div class="card-item">
+                <div class="card-header">
+                    <p>Wooza</p>
+                    <h2>20GB</h2>
+                </div>
+                <div class="card-content">
+                    <div class="row-benefit">
+                        <p>Traga seu número para Wooza</p>
+                        <h4><strong>2</strong> de internet</h4>
+                    </div>
+                    <div class="price">
+                        <span class="currency">R$</span>
+                        <span class="big-price">99</span>
+                        <div class="wrap">
+                            <span class="cents">,99</span>
+                            <span class="month">/Mês</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer"></div>
+            </div>
+        </div>
     </div>
 );
 
