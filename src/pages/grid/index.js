2954d28a9a8dbe1726141d6e20554a44f7a27900
Pedro Rique <pedro.rique@wooza.com.br>
2019-08-26 11:43:01 -0300

    feat: inicio da estrutura grid

--- src/pages/grid/index.js
+++ src/pages/grid/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import Card from '../../components/card';
+
+import './style.scss';
+
+const plans = require('../../plans.json');
+
 const Grid = () => (
-    <div>
+    <section className='grid'>
         <h1>Grid Layout</h1>
-    </div>
+        <div className='cards'>
+            {
+                plans.map(p => {
+                    return (<Card plan={p}/>)
+                })
+            }
+        </div>
+    </section>
 );
 
 export default Grid;
